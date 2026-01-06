93982fa6adeb731ed518bd72b18d31d376b8e904
wake-up-studio <wakeuphuis@gmail.com>
2026-01-06 11:31:23 +0000

    fin exercice 1

---
+++ exercice-variables/index.js
@@ -0,0 +1,26 @@
+const character = "Legolas";
+
+console.log(character);
+
+let name = "Legolas";
+
+console.log(name);
+
+name = "Gimli";
+
+console.log(name);
+
+let large = "35000200042060500";
+console.log(large);
+
+let decimal = "0.6543654638313546434314354365";
+console.log(decimal);
+
+let negatif = "-2";
+console.log(negatif);
+
+let truth = "yes";
+console.log(truth);
+
+let lie = "no";
+console.log(lie);
